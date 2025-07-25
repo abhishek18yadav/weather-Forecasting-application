ebab00b7ef6b90750518ad734f2f56cb14ae693c
Abhishek <150270427+abhishek18yadav@users.noreply.github.com>
2025-07-25 11:27:33 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Display from "./component/Display";
+
+
+function App(){
+  return(
+    <div className="flex justify-center items-center pt-23 ">
+      <Display/>
+    </div>
+    
+  );
+}
+export default App;
