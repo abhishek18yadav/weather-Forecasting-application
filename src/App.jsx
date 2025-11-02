b70285425b3b323af59ad862fa8fef29aae2cc56
abhishek <abhishek8046778@gmail.com>
2025-11-02 08:30:53 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Display from "./component/Display";
 
 
-function App(){
+function App() {
+  console.log("hello");
   return(
     <div className="flex justify-center items-center pt-23 ">
       <Display/>
